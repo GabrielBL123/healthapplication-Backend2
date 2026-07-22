97f13a20f3e9769e9967b178be651fa71d8a8094
gabri <gabriel.lamezon@outlook.com>
2026-07-22 08:28:43 -0300

    22/07

--- src/main/java/com/gabrielbl/healthaplication/repository/RefreshTokenRepository.java
+++ src/main/java/com/gabrielbl/healthaplication/repository/RefreshTokenRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
+import java.util.UUID;
 
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
 
 
      Optional<RefreshToken> findByTokenHash(String tokenHash);
